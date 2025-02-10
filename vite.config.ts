633b14ca2a9a8a27b21a5c9af20835076ac4d908
Oleksandr Dzhychko <oleksandr.dzhychko@fernuni-hagen.de>
2025-02-10 15:12:00 +0100

    build: configure Vite to recognize custom elements

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    }),
     vueJsx(),
     vueDevTools(),
   ],
