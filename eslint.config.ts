aa518b6585cf259c8a851117cd550dbf01f7718f
Oleksandr Dzhychko <ooleksandr.dzhychko@fernuni-hagen.de>
2025-03-07 16:46:35 +0000

    ci: do not lint checked in aig_graph_component

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,17 +17,17 @@ export default defineConfigWithVueTs(
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/public/aig_graph_component/**'],
   },
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
