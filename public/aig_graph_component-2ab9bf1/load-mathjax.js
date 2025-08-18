9fd0d74913b1555298389ed1eb0c28b0006f658f
Oleksandr Dzhychko <oleksandr.dzhychko@fernuni-hagen.de>
2025-08-18 12:13:43 +0200

    chore: update graph component version

--- public/aig_graph_component-2ab9bf1/load-mathjax.js
+++ public/aig_graph_component-2ab9bf1/load-mathjax.js
@@ -4,20 +4,20 @@
 // Before enabling this, we should check how LaTex can be correctly rendered in all UI elements that show node names outside of the graph.
 
 // window.MathJax = {
-//   options: { enableMenu: false },
-//   tex: {
-//       inlineMath: [
-//           ['$', '$'],
-//           ['\\(', '\\)']
-//       ]
-//   },
-//   svg: {
-//       fontCache: 'global'
-//   }
+//     options: { enableMenu: false },
+//     tex: {
+//         inlineMath: [
+//             ['$', '$'],
+//             ['\\(', '\\)']
+//         ]
+//     },
+//     svg: {
+//         fontCache: 'global'
+//     }
 // }
 // ;(function () {
-//   let script = document.createElement('script')
-//   script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js'
-//   script.async = true
-//   document.head.appendChild(script)
+//     let script = document.createElement('script')
+//     script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js'
+//     script.async = true
+//     document.head.appendChild(script)
 // })()
