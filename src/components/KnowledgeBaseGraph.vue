4e0ed51c4d3fbd703c7acb176e008a923f80ecd6
Oleksandr Dzhychko <oleksandr.dzhychko@fernuni-hagen.de>
2025-09-13 15:44:54 +0200

    test: setup browser tests with screenshots

--- src/components/KnowledgeBaseGraph.vue
+++ src/components/KnowledgeBaseGraph.vue
@@ -1,28 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import '@/assets/aig_graph_component-2ab9bf1/graph-component.js'
+import '@/assets/aig_graph_component-2ab9bf1/graph-component.css'
+</script>
 
 <template>
-  <graph-component></graph-component>
+  <graph-component />
 </template>
 
-<style scoped>
-graph-component {
-  display: block;
-  height: 100%;
-}
-</style>
-
 <style>
-.graph-controller__graph-host svg {
-  /*
-    <svg> is by default "display: inline" whicht causes a slight overflow.
-    See https://stackoverflow.com/questions/72732175/svg-set-to-100-height-causes-overflow-in-div-why
-  */
-  display: block;
-  /*
-    Overide CSS properites set by Bulma, which in turn override "width" and "height" attribute on the <svg> element.
-  */
-  width: 100%;
-  height: 100%;
+.graph-controller__graph-host {
+  position: static;
 }
 
 .graph-controller__controls-overview {
