6938d4baeaf45b9fc42b92812dec8d239fc0fa0d
Oleksandr Dzhychko <hey@oleks.dev>
2026-01-20 11:44:47 +0100

    chore: fix license header for Vue files

--- src/thirdPartyLicenses/ThirdPartyLicensesView.vue
+++ src/thirdPartyLicenses/ThirdPartyLicensesView.vue
@@ -15,7 +15,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *-->
+-->
 <script setup lang="ts">
 import attributionsUntyped from '@/../third-party/attribution.json'
 import type { Attribution } from './types'
