dc357b0c2391e653718e70473b1fc4b73330bb81
Oleksandr Dzhychko <hey@oleks.dev>
2026-02-22 13:26:21 +0100

    chore: remove unneeded comments

--- src/modules/common/stores/notifications.ts
+++ src/modules/common/stores/notifications.ts
@@ -19,8 +19,6 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-// XXX consider binding notifications to component instead of making them global.
-
 interface Notification {
   id: number
   message: string
